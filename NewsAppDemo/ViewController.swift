e5d875f6ed96742f7ce77d56993125153077242c
Avinash <avinash.gupta@hashedin.com>
2020-07-12 11:39:36 +0530

    Initial commit

---
+++ NewsAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppDemo
+//
+//  Created by hasher on 11/07/20.
+//  Copyright © 2020 Avinash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
